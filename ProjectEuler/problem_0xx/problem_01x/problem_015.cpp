801af124323cc68b63047cfb8d5a005d29094f21
Kid80 <69387556+Kid80@users.noreply.github.com>
2025-04-20 14:46:08 +0100

    Stuff

---
+++ ProjectEuler/problem_0xx/problem_01x/problem_015.cpp
@@ -0,0 +1,7 @@
+#include "problem_01x.hpp"
+
+uint64_t problem_015(int grid_size) {
+	uint64_t num_possible = 0;
+
+	for(uint64_t i = 0; i < ((uint64_t)1 << grid_size))
+}
--- ProjectEuler/problem_0xx/problem_01x/problem_01x.hpp
+++ ProjectEuler/problem_0xx/problem_01x/problem_01x.hpp
@@ -2,3 +2,6 @@
 #include <cstdint>
 
 uint64_t problem_012(int target_num_factors);
+
+
+uint64_t problem_015(int grid_size);
